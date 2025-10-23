196619ef03bd02876445f1563368ead4f3bae0fe
Mahmoud Elshabrawy <mahmoudwaell391@gmail.com>
2025-10-23 15:52:15 +0300

    Complete SubCategories CRUD Operations and routes and validations

--- controllers/subCategoryController.js
+++ controllers/subCategoryController.js
@@ -3,6 +3,36 @@ const AppError = require("../utils/appError");
 const asyncHandler = require("express-async-handler");
 const slugify = require("slugify");
 
+exports.getSubCategories = asyncHandler(async (req, res) => {
+  const page = req.query.page * 1 || 1;
+  const limit = req.query.limit * 1 || 10;
+  const skip = (page - 1) * limit;
+  const subCategories = await SubCategory.find()
+    .skip(skip)
+    .limit(limit)
+    .populate({ path: "category", select: "name -_id" });
+  res.status(200).json({
+    status: "success",
+    results: subCategories.length,
+    data: subCategories,
+  });
+});
+
+exports.getSubCategory = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  const subCategory = await SubCategory.findById(id).populate({
+    path: "category",
+    select: "name",
+  });
+  if (!subCategory) {
+    return next(new AppError(`No category found with this ID: ${id}`, 404));
+  }
+  res.status(200).json({
+    status: "success",
+    data: subCategory,
+  });
+});
+
 exports.createSubCategory = asyncHandler(async (req, res) => {
   const { name, category } = req.body;
   const subCategory = await SubCategory.create({
@@ -14,5 +44,37 @@ exports.createSubCategory = asyncHandler(async (req, res) => {
   res.status(201).json({
     status: "success",
     data: subCategory,
+  });
+});
+
+exports.updateSubCategory = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  const name = req.body.name;
+  const subCategory = await SubCategory.findByIdAndUpdate(
+    id,
+    { name, slug: slugify(name) },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+  if (!subCategory) {
+    return next(new AppError(`No SubCategory found with this ID: ${id}`, 404));
+  }
+  res.status(200).json({
+    status: "success",
+    data: subCategory,
+  });
+});
+
+exports.deleteSubCategory = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  const subCategory = await SubCategory.findByIdAndDelete(id);
+  if (!subCategory) {
+    return next(new AppError(`No SubCategory found with this ID: ${id}`, 404));
+  }
+  res.status(204).json({
+    status: "success",
+    data: null,
   });
 });
--- routes/subCategoryRoutes.js
+++ routes/subCategoryRoutes.js
@@ -10,7 +10,15 @@ const {
 const router = express.Router();
 
 router
-  .route("/")
+  .route('/')
+  .get(subCategoryController.getSubCategories)
   .post(createSubCategoryValidator, subCategoryController.createSubCategory);
+
+
+router
+  .route('/:id')
+  .get(getSubCategoryValidator, subCategoryController.getSubCategory)
+  .patch(updateSubCategoryValidator, subCategoryController.updateSubCategory)
+  .delete(deleteSubCategoryValidator, subCategoryController.deleteSubCategory)
 
 module.exports = router;
