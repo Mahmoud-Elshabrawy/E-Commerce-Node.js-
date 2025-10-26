ec28cd3583f53f140954a0ad0db564724503206b
Mahmoud Elshabrawy <mahmoudwaell391@gmail.com>
2025-10-26 16:35:10 +0300

    Implement Sorting , Field Limiting and Searching API Features

--- controllers/productController.js
+++ controllers/productController.js
@@ -6,21 +6,59 @@ const qs = require('qs')
 
 
 exports.getProducts = asyncHandler(async (req, res) => {
+  // 1) Filtration
   const queryStringObj = qs.parse(req.query)
-  const excludedFields = ["page", "sort", "limit", "fields"];
+  const excludedFields = ["page", "sort", "limit", "fields", "keyword"];
   excludedFields.forEach((el) => delete queryStringObj[el]);
+
   // Apply filtering using [gt, gte, lt, lte]
   let queryStr = JSON.stringify(queryStringObj)
   queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g , match => `$${match}`)
   
-  
+  // 2) Pagination
   const page = req.query.page * 1 || 1;
   const limit = req.query.limit * 1 || 10;
   const skip = (page - 1) * limit;
-  const products = await Product.find(JSON.parse(queryStr))
+
+  
+  // Build Query
+  let Query = Product.find(JSON.parse(queryStr))
     .skip(skip)
     .limit(limit)
     .populate({ path: "category", select: "name -_id" });
+    
+    
+    // 3) Sorting
+    if (req.query.sort) {
+      let sortBy = req.query.sort.split(',').join(' ')
+      
+      console.log(sortBy);
+      
+      Query = Query.sort(sortBy)
+    } else {
+      Query = Query.sort('-createdAt')
+    }
+    
+    // 4) Field Limiting
+    if(req.query.fields) {
+    let fields = req.query.fields.split(',').join(' ')
+    Query = Query.select(fields)
+  } else {
+    Query = Query.select('-__v')
+  }
+  
+  // 5) Search
+  if(req.query.keyword) {
+    let keyword = {}
+    keyword.$or = [
+      {name: {$regex: req.query.keyword, $options: 'i'}},
+      {description: {$regex: req.query.keyword, $options: 'i'}}
+    ]
+    Query = Query.find(keyword)
+  }
+
+  // Execute Query
+  const products = await Query
   
     res.status(200).json({
     status: "success",
