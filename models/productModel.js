7ec91ed7532738d04c96355f1ca508af4ccf986f
Mahmoud Elshabrawy <mahmoudwaell391@gmail.com>
2025-10-29 23:36:06 +0300

    return product imgCover url and images url on resopnse

--- models/productModel.js
+++ models/productModel.js
@@ -1,73 +1,98 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
+const productSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
-        unique: [true, 'Product must be unique'],
-        trim: true,
-        minlength: [3, 'Too short product title'],
-        maxlength: [100, 'Too long product title']
+      type: String,
+      required: true,
+      unique: [true, "Product must be unique"],
+      trim: true,
+      minlength: [3, "Too short product title"],
+      maxlength: [100, "Too long product title"],
     },
     slug: {
-        type: String,
-        lowercase: true
+      type: String,
+      lowercase: true,
     },
     description: {
-        type: String,
-        required: [true, 'Product Description required'],
-        minlength: [20, 'Too short product description'],
+      type: String,
+      required: [true, "Product Description required"],
+      minlength: [20, "Too short product description"],
     },
     price: {
-        type: Number,
-        required: [true, 'product price required'],
-        trim: true
+      type: Number,
+      required: [true, "product price required"],
+      trim: true,
     },
-    priceAfterDiscount: Number
-    ,
+    priceAfterDiscount: Number,
     quantity: {
-        required: [true, 'product quantity required'],
-        type: Number
+      required: [true, "product quantity required"],
+      type: Number,
     },
     sold: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0,
     },
     colors: [String],
     imageCover: {
-        type: String,
-        required: [true, 'product must have an imageCover']
+      type: String,
+      required: [true, "product must have an imageCover"],
     },
     images: [String],
     category: {
-        type: mongoose.Types.ObjectId,
-        ref: 'Category',
-        required: [true, 'Product must belong to Category']
+      type: mongoose.Types.ObjectId,
+      ref: "Category",
+      required: [true, "Product must belong to Category"],
     },
-    subcategories:[{
+    subcategories: [
+      {
         type: mongoose.Types.ObjectId,
-        ref: 'SubCategory',
-    }],
+        ref: "SubCategory",
+      },
+    ],
     brand: {
-        type: mongoose.Types.ObjectId,
-        ref: 'Brand',
+      type: mongoose.Types.ObjectId,
+      ref: "Brand",
     },
     ratingsAverage: {
-        type: Number,
-        min: [1, 'Rating must be above or equal 1.0'],
-        max: [5, 'Rating must be below or equal 5.0']
+      type: Number,
+      min: [1, "Rating must be above or equal 1.0"],
+      max: [5, "Rating must be below or equal 5.0"],
     },
     ratingsQuantity: {
-        type: Number,
-        default: 0
-    }
-}, {
-    timestamps: true
-})
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 // Query Middleware
-productSchema.pre(/^find/, function(next) {
-    this.populate({path: 'category', select: 'name'})
-    next()
-})
-const ProductModel = mongoose.model('Product', productSchema)
-module.exports = ProductModel
+productSchema.pre(/^find/, function (next) {
+  this.populate({ path: "category", select: "name" });
+  next();
+});
+
+const setImageCoverUrl = (doc) => {
+  if (doc.imageCover) {
+    const imageCoverUrl = `${process.env.baseUrl}/products/${doc.imageCover}`;
+    doc.imageCover = imageCoverUrl;
+  }
+  if (doc.images && Array.isArray(doc.images)) {
+    doc.images = doc.images.map(
+      (img) => `${process.env.baseUrl}/products/${img}`
+    );
+  }
+};
+
+productSchema.pre("init", function (doc) {
+  setImageCoverUrl(doc);
+});
+
+productSchema.pre("save", function () {
+  setImageCoverUrl(this);
+});
+
+const ProductModel = mongoose.model("Product", productSchema);
+module.exports = ProductModel;
