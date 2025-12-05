efcb04ca5ded398fc0a9c6167f0ed9eb9755567e
Mahmoud Elshabrawy <mahmoudwaell391@gmail.com>
2025-12-06 01:40:57 +0200

    implement hpp to protect against HTTP Parameter Pollution attacks and sanitize date to prevent scripts and no sql query injection

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const cors = require('cors')
 const compression = require('compression')
 const rateLimit = require('express-rate-limit')
 const hpp = require('hpp');
+const mongoSanitize = require('mongo-sanitize');
+const xss = require('xss');
 
 
 const mountRoutes = require('./routes/index')
@@ -39,6 +41,56 @@ app.post('/webhook-checkout', express.raw({type: 'application/json'}), webhookCh
 
 // set request size limit to 20kb
 app.use(express.json({limit: '20kb'}))
+
+
+// middleware to protect against HTTP Parameter Pollution attacks
+app.use(hpp({
+    // for testing
+    whitelist : [ 'price', 'sold', 'quantity', 'ratingsAverage', 'ratingsQuantity']
+}))
+
+
+// Sanitize Data
+
+// prevent scripts
+app.use((req, res, next) => {
+    if(req.body) {
+        for (const key in req.body) {
+            if(typeof req.body[key] === 'string') {
+                req.body[key] = xss(req.body[key])
+            }
+        }
+    }
+    if (req.query) {
+        for (const key in req.query) {
+             if (typeof req.query[key] === 'string') {
+                req.query[key] = xss(req.query[key]);
+            }
+        }
+    }
+    
+    next();
+})
+
+// to prevent no sql query injection
+app.use((req, res, next) => {
+    if (req.body) {
+        req.body = mongoSanitize(req.body); 
+    }
+    next();
+});
+
+// Limit each IP to 100 requests per `window` (here, per 15 minutes).
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, 
+    limit: 100, 
+    message: 'Too Many Requests from this IP, Please try again in 15 minutes',
+})
+
+
+// Apply the rate limiting middleware to all requests.
+app.use('/api', limiter)
+
 app.use(express.static(path.join(__dirname, 'uploads')))
 
 if(process.env.NODE_ENV === 'development') {
@@ -46,21 +98,6 @@ if(process.env.NODE_ENV === 'development') {
     // console.log(`The Current Mode is: ${process.env.NODE_ENV}`);
 }
 
-// Limit each IP to 100 requests per `window` (here, per 15 minutes).
-const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, 
-	limit: 100, 
-	message: 'Too Many Requests from this IP, Please try again in 15 minutes',
-})
-
-// Apply the rate limiting middleware to all requests.
-app.use('/api', limiter)
-
-// middleware to protect against HTTP Parameter Pollution attacks
-app.use(hpp({
-    // for testing
-    whitelist : [ 'sort']
-}))
 
 // Routes
 mountRoutes(app)
